f57c6beda9cb8a5c1e699fa266c9f9aa615d4c05
Lucassantos000 <48740555+Lucassantos000@users.noreply.github.com>
2023-01-03 18:48:49 -0400

    animações responsivas da top ba

--- js/index/busca.js
+++ js/index/busca.js
@@ -27,3 +27,91 @@ document.getElementById('buscaHomeMenor').addEventListener('blur', (e)=>{
 
 })
 
+
+
+
+//ANIIMAÇÃO PARA O CONTAINER DOS ELEMENTOS DA TOP BAR RESPOSNVIA APARECEREM
+
+function abreContainerTopBarResponsivo(){
+
+
+    console.log(document.getElementById('barraResponsiva').getAttribute('aberto'));
+
+
+    if(document.getElementById('barraResponsiva').getAttribute('aberto') == 'false'){
+        console.log('abre');
+        document.getElementById('spanIconResponsiveFechar').style.display= 'flex';
+        document.getElementById('spanIconResponsiveAbrir').style.display= 'none';
+
+        $(".contentElementsBarResponsive").animate({
+            height: "290px"
+        }, ()=>{
+        
+        });
+        document.getElementById('barraResponsiva').setAttribute('aberto','true');
+        document.getElementsByClassName('contentElementsBarResponsive')[0].style.height = 'auto';
+        // $(".contentElementsBarResponsive").css('height','auto');
+        // document.getElementsByClassName('contentElementsBarResponsive')[0].style.height='auto';
+        
+    }else{
+        document.getElementById('spanIconResponsiveAbrir').style.display= 'flex';
+        document.getElementById('spanIconResponsiveFechar').style.display= 'none';
+
+        console.log('fecha');
+        $(".contentElementsBarResponsive").animate({
+            height: "0px"
+        }, 200, ()=>{
+        });
+        document.getElementById('barraResponsiva').setAttribute('aberto','false')
+
+    }
+
+  
+}
+
+
+
+//ANIMAÇÃO PARA OS SUBELEMEETOS DA CATEGORIA INSTITUICIONAL
+function mostraSubElementsInstitucional(){
+    
+if(document.getElementById('wrapingSubElementsInstitucional').getAttribute('aberto') == 'false'){   
+    document.getElementById('rowElementInstitucinalResponsive').style.rotate = '180deg';
+    $("#wrapingSubElementsInstitucional").animate({
+        height: "90px"
+    }, 400, ()=>{
+    document.getElementById('wrapingSubElementsInstitucional').setAttribute('aberto','true')
+    });
+}else{
+    document.getElementById('rowElementInstitucinalResponsive').style.rotate = '0deg';
+        $("#wrapingSubElementsInstitucional").animate({
+        height: "0px"
+    }, 400, ()=>{
+    document.getElementById('wrapingSubElementsInstitucional').setAttribute('aberto','false')
+    });
+
+}
+
+}
+
+
+//ANIMAÇÃO PARA OS SUBELEMENTOS DA CATEGORIA SOLICITACAO
+function mostraSubElementsSolicitacao(){
+    
+    if(document.getElementById('wrapingSubElementsSolicitacao').getAttribute('aberto') == 'false'){   
+        document.getElementById('rowElementSolicitacaoResponsive').style.rotate = '180deg';
+        $("#wrapingSubElementsSolicitacao").animate({
+            height: "30px"
+        }, 400, ()=>{
+        document.getElementById('wrapingSubElementsSolicitacao').setAttribute('aberto','true')
+        });
+    }else{
+        document.getElementById('rowElementSolicitacaoResponsive').style.rotate = '0deg';
+            $("#wrapingSubElementsSolicitacao").animate({
+            height: "0px"
+        }, 400, ()=>{
+        document.getElementById('wrapingSubElementsSolicitacao').setAttribute('aberto','false')
+        });
+    
+    }
+    
+    }
--- js/index/maisNoticias.js
+++ js/index/maisNoticias.js
@@ -2,6 +2,23 @@
 async function maisNoticias(element){
     console.log(element);
 
-    // O ATRIBUTO PAGINATION SERVE PARA MOSTAR A PAGINAÇÃO DAS NOTÍCIA NA PÁGINA, (INICIALMENTE )
-    element.getAttribute('pagination');
+    // O ATRIBUTO PAGINATION SERVE PARA MOSTAR A PAGINAÇÃO DAS NOTÍCIA NA PÁGINA, (INICIALMENTE SÃO CARREGADAS 5 NOTÍCIAS ) E NAS SEGUNTAS MAIS 5 EM CASA ATÉ QUE NA ÚLTIMA VAI SER REDIRECIONADA PRA PÁGINA DE NOTÍCIAS 
+    console.log(element.getAttribute('pagination'));
+
+
+    //ATUALIAZAR O VALOR DA PAGINAÇÃO
+    let paginationValue = (parseInt(element.getAttribute('pagination'))+1).toString();
+    // console.log()
+    element.setAttribute('pagination',paginationValue);
+    console.log(element.getAttribute('pagination'));
+
+
+    if(element.getAttribute('pagination')==='2'){
+    // COMANDO PARA REDIRECIONAR PARRA UMA NOVA PÁGINA
+    const url = '/pages/noticias.html' 
+    const win = window.open(url, '_blank');
+    win.focus();
+
+    }
+
 }
