5a0bf8cc1d0e7e06bd44f043ec380e6be56aae93
Lucassantos000 <48740555+Lucassantos000@users.noreply.github.com>
2023-01-03 15:04:24 -0400

    Add files via upload

--- js/index/maisNoticias.js
+++ js/index/maisNoticias.js
@@ -1,4 +1,7 @@
 
 async function maisNoticias(element){
     console.log(element);
+
+    // O ATRIBUTO PAGINATION SERVE PARA MOSTAR A PAGINAÇÃO DAS NOTÍCIA NA PÁGINA, (INICIALMENTE )
+    element.getAttribute('pagination');
 }
